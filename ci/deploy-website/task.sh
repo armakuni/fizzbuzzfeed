ea1000737cb5730aaaf57e3b65049a8c1a5eec82
Fabio Ferrante <fabioferrante@Fabios-MacBook-Pro.local>
2019-08-22 13:31:06 +0100

    updated sh file

--- ci/deploy-website/task.sh
+++ ci/deploy-website/task.sh
@@ -8,6 +8,8 @@ gem uninstall bundler -x
 gem install bundler
 gem cleanup
 bundle -v
+bundle update --bundler
+bundle -v
 jekyll build
 cp _site/* ../generated-site/
 cp manifest.yml ../generated-site/
