49bfd17a8b56c3a235da01e3c77806cf59cd9639
Benedict Steele <benedict.steele@armakuni.com>
2019-08-22 17:07:02 +0100

    renaming github-repo to articles

--- ci/deploy-website/task.sh
+++ ci/deploy-website/task.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set +ex
-chown jekyll github-repo/
-cd github-repo
+chown jekyll articles/
+cd articles
 jekyll build
 cp -a _site/ ../generated-site/
 cp manifest.yml ../generated-site/
