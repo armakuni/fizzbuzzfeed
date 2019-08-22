ed55817abba84817a432437d5a60bfcc71c35cb5
Fabio Ferrante <fabioferrante@Fabios-MacBook-Pro.local>
2019-08-22 13:12:11 +0100

    updated sh file

--- ci/deploy-website/task.sh
+++ ci/deploy-website/task.sh
@@ -5,8 +5,8 @@ bundle -v
 grep -A 1 "BUNDLED WITH" Gemfile.lock
 gem update --system
 bundle -v
-gem uninstall bundler
 gem install bundler
+gem cleanup
 bundle -v
 jekyll build
 cp _site/* ../generated-site/
