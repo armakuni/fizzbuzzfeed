836049f42864ed43cb4e80ce35749dbda6720a84
Fabio Ferrante <fabioferrante@Fabios-MacBook-Pro.local>
2019-08-22 13:02:01 +0100

    updated sh file

--- ci/deploy-website/task.sh
+++ ci/deploy-website/task.sh
@@ -3,6 +3,10 @@ set +ex
 cd github-repo
 bundle -v
 grep -A 1 "BUNDLED WITH" Gemfile.lock
+gem update --system
+bundle -v
+gem install bundler
+bundle -v
 jekyll build
 cp _site/* ../generated-site/
 cp manifest.yml ../generated-site/
