9f6b5286ee16125382cae4ea23ca728b883bdb81
Fabio Ferrante <fabioferrante@Fabios-MacBook-Pro.local>
2019-08-22 15:03:28 +0100

    update sh file

--- ci/deploy-website/task.sh
+++ ci/deploy-website/task.sh
@@ -3,6 +3,6 @@ set +ex
 chown jekyll github-repo/
 cd github-repo
 jekyll build
-cp _site ../generated-site/
+cp -a _site/ ../generated-site/
 cp manifest.yml ../generated-site/
 cp Staticfile ../generated-site/
