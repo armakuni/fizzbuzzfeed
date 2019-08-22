dd982dd17d6f288356b8993ae95a8db0090d7797
Fabio Ferrante <fabioferrante@Fabios-MacBook-Pro.local>
2019-08-22 12:54:11 +0100

    updated sh file

--- ci/deploy-website/task.sh
+++ ci/deploy-website/task.sh
@@ -2,10 +2,6 @@
 set +ex
 cd github-repo
 bundle -v
-gem update --system
-gem install bundler
-bundler update --bundler
-bundle -v
 jekyll build
 cp _site/* ../generated-site/
 cp manifest.yml ../generated-site/
