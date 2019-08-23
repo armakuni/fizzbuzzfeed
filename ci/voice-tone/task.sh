ae522daf48285a2c0284b650caf6f5a500ba81cd
Julian Nonino <noninojulian@gmail.com>
2019-08-23 14:41:29 +0100

    Avoiding checking passive voice and fixing weasel words

--- ci/voice-tone/task.sh
+++ ci/voice-tone/task.sh
@@ -6,15 +6,4 @@ set +ex
 
 cd articles/_posts
 
-write-good *.md --parse
-
-
-# for i in $(find . -iname '*.md'); do
-#     OUTPUT=$(alex $i 2>&1)
-#     echo $OUTPUT
-#     if [[ $OUTPUT != *"no issues found"* ]]
-#     then        
-#         EXITCODE=1
-#     fi
-# done
-# exit $EXITCODE
+write-good *.md --parse --no-passive
