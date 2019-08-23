64bb9bb695a3e284582841c13f2a5f4e6961b17c
Julian Nonino <noninojulian@gmail.com>
2019-08-23 12:38:25 +0100

    Trying to introduce a warning for voice tone checker

--- ci/voice-tone/task.sh
+++ ci/voice-tone/task.sh
@@ -6,7 +6,14 @@ set +ex
 EXITCODE=0
 cd articles/_posts
 for i in $(find . -iname '*.md'); do
-    alex $i
-    
+    OUTPUT='alex $i'
+    if [[ $OUTPUT == *"no issues found"* ]]
+    then
+        EXITCODE=0
+    else
+    then
+        echo $OUTPUT
+        EXITCODE=1
+    fi
 done
 exit $EXITCODE
