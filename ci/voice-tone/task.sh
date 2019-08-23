077fac237d55aa4f0e16fe44eaced85c897d6e90
Julian Nonino <noninojulian@gmail.com>
2019-08-23 12:43:57 +0100

    Fixing error in voice-tone checker

--- ci/voice-tone/task.sh
+++ ci/voice-tone/task.sh
@@ -6,7 +6,7 @@ set +ex
 EXITCODE=0
 cd articles/_posts
 for i in $(find . -iname '*.md'); do
-    OUTPUT='alex $i'
+    OUTPUT=$(alex $i)
     echo $OUTPUT
     if [[ $OUTPUT == *"no issues found"* ]]
     then        
