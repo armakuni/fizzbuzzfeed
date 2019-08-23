39541f0ad10c589512d66044dffb844c4b43d000
Julian Nonino <noninojulian@gmail.com>
2019-08-23 14:16:40 +0100

    Use markdown-link-check

--- ci/link-validation/task.sh
+++ ci/link-validation/task.sh
@@ -8,7 +8,7 @@ npm install -g markdown-link-check
 
 for MARKDOWN_FILE in $(find . -iname '*.md')
 do
-  node_modules/markdown-link-check/markdown-link-check \
+  markdown-link-check \
     --config="../ci/link-validation/checklink-config.json" \
     "$MARKDOWN_FILE"
 done
