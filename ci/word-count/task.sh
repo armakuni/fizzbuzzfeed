a14bc9f582220d79fdb926019151c19276242bed
Timothy Long <tim.long@armakuni.com>
2019-08-28 16:28:25 +0100

    make pipeline basic again, word count 45-55

--- ci/word-count/task.sh
+++ ci/word-count/task.sh
@@ -5,11 +5,11 @@ cd articles/_posts
 for i in $(find . -iname '*.md'); do
     WORDCOUNT=`cat "$i" | tail +6 | wc -w`
     echo "$i has $WORDCOUNT words"
-    if [ $WORDCOUNT -gt 100 ]
+    if [ $WORDCOUNT -gt 55 ]
     then
         echo "$i contains too many words!"
         EXITCODE=1
-    elif [ $WORDCOUNT -lt 80 ]
+    elif [ $WORDCOUNT -lt 45 ]
     then
         echo "$i contains too few words!"
         EXITCODE=1
